f2e8cdfdfa798c3486fef03ad74ae77b1493e2cd
Niyati Sompura <niaytisompura03@gmail.com>
2026-04-07 23:12:56 +0530

    feat: add Inngest functions to sync Clerk user lifecycle events with the database

--- config/inngest.js
+++ config/inngest.js
@@ -10,7 +10,7 @@ export const syncUserCreation = inngest.createFunction(
     {
         id : 'sync-user-from-clerk'
     },
-    { event : 'clerk/user.created'},
+    { event : 'clerk/currentUser.created'},
     async ({event}) => {
         const  { id, first_name, last_name, email_addresses, image_url} = event.data
 
@@ -30,7 +30,7 @@ export const syncUserUpdation = inngest.createFunction(
     {
         id : 'update-user-from-clerk'
     },
-    { event : 'clerk/user.updated'},
+    { event : 'clerk/currentUser.updated'},
     async ({event}) => {
         const  { id, first_name, last_name, email_addresses, image_url} = event.data
 
@@ -50,7 +50,7 @@ export const syncUserDeletion = inngest.createFunction(
     {
         id : 'delete-user-with-clerk'
     },
-    { event: 'clerk/user.deleted'},
+    { event: 'clerk/currentUser.deleted'},
     async ({event}) =>{
         const { id } = event.data
         await connectDB()
